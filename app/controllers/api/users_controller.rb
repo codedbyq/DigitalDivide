7665a902369b0cf1eb52d153fa19b70258587ceb
Marquis Bentley <livefreekai@icloud.com>
2020-12-05 14:25:14 -0700

    Test and debug the user api calls

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,7 +5,7 @@ class Api::UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:user_id])
+        @user = User.find(params[:id])
         render 'api/users/show'
     end
 
