7c7005a6069d71280bb4848ce3484a1d5aee538a
Marquis Bentley <livefreekai@icloud.com>
2020-12-05 19:27:38 -0700

    Create the session reducer and load it into the root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import EntitiesReducer from './entities/entities_reducer';
+import SessionReducer from './session/session_reducer';
 
-const RootReducer = combineReducer({
-    entities: EntitiesReducer
+const RootReducer = combineReducers({
+    entities: EntitiesReducer,
+    session: SessionReducer
 });
 
 export default RootReducer;
