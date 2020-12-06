d7d41e3dd45561338d09663cb341564338902a7a
Marquis Bentley <livefreekai@icloud.com>
2020-12-05 19:35:51 -0700

    Setup the react entry file with a preloaded state if a user is logged in and initialize the store

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Digital Divide</h1>, root)
+    let preloadedState = undefined;
+
+    // initial state with a logged in user
+    if (window.currentUser) {
+        preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            }, 
+            session: { currentUser: window.currentUser.id }
+        };
+
+        delete window.currentUser;
+    }
+
+    const store = configureStore(preloadedState);
+    ReactDOM.render(<Root store={store} />, root);
 
     //! Testing Remove before pushing for production
-    
 });
